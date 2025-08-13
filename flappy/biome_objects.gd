190c55cc34625234f3cc36fb68a486319c7b53f9
Daniel Hall <git@danielhall.me>
2025-08-13 21:18:24 +1000

    Inifinite scrolling level

--- flappy/biome_objects.gd
+++ flappy/biome_objects.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
-@onready var ground_sprite: Sprite2D = $Ground/Sprite2D
-@onready var up_sprite: Sprite2D = $Up/Sprite2D
-@onready var down_sprite: Sprite2D = $Down/Sprite2D
+@export var width :float = 806.0
 
-func _ready() -> void:
-	pass
+@onready var ground_sprite: Sprite2D = $Ground/Sprite2D
+
+@onready var up_sprite: Sprite2D = $Up/Sprite2D
+@onready var up: StaticBody2D = $Up
+
+@onready var down_sprite: Sprite2D = $Down/Sprite2D
+@onready var down: StaticBody2D = $Down
+
+const obstacle_min_x := 100
+const obstacle_max_x := 700
+
+func set_obstacle_positions() -> void:
+	up.position.x = randf_range(obstacle_min_x, obstacle_max_x)
+	down.position.x = randf_range(obstacle_max_x, obstacle_max_x)
 
 func set_biome(new_biome: Biome) -> void:
 	ground_sprite.texture = new_biome.ground
--- flappy/level.gd
+++ flappy/level.gd
@@ -1,3 +1,52 @@
 extends Node2D
 
 @export var biome_scene: PackedScene = preload("res://biome_objects.tscn")
+
+const speed := 100
+const BIOMES := [
+	preload("res://Resources/Biomes/dirt.tres"),
+	preload("res://Resources/Biomes/ice.tres"),
+	preload("res://Resources/Biomes/rock.tres"),
+	preload("res://Resources/Biomes/snow.tres"),
+]
+
+@onready var biome_scenes := $BiomeScenes
+
+var biome_instances: Array = []
+
+func _ready() -> void:
+	randomize()
+
+	var add_at_x := 0.0
+
+	for i in range(3):
+		var instance = biome_scene.instantiate()
+
+		biome_instances.append(instance)
+		biome_scenes.add_child(instance)
+
+		instance.set_obstacle_positions()
+		instance.set_biome(BIOMES[randi() % BIOMES.size()])
+
+		instance.position.x = add_at_x
+		add_at_x += instance.width
+
+func _physics_process(delta: float) -> void:
+	var to_remove: Array = []
+
+	for instance in biome_instances:
+		instance.position.x -= speed * delta
+
+		if instance.position.x < -instance.width:
+			to_remove.append(instance)
+
+	for instance in to_remove:
+		biome_instances.erase(instance)
+		
+		instance.set_obstacle_positions()
+		instance.set_biome(BIOMES[randi() % BIOMES.size()])
+
+		var last_biome := biome_instances.size() - 1
+		instance.position.x = biome_instances[last_biome].position.x + biome_instances[last_biome].width
+
+		biome_instances.append(instance)
