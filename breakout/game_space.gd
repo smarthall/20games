a448adb995e9238d11c729c104087c076184deb5
Daniel Hall <git@danielhall.me>
2025-08-15 22:24:23 +1000

    Exit on Escape

--- breakout/game_space.gd
+++ breakout/game_space.gd
@@ -7,6 +7,9 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Action") && ball.get_parent() == ball_holding_node:
 		launch_ball()
 
+	elif event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
 func launch_ball() -> void:
 	ball.reparent(self)
 	ball.start()
