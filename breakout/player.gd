4c5daf608127ef0205f5ec764143ea901e02908d
Daniel Hall <git@danielhall.me>
2025-08-16 17:24:46 +1000

    Simpler paddle handling

--- breakout/player.gd
+++ breakout/player.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
 @export var speed: float = 400.0
+@export var paddle_margin: float = 10.0
+
+@onready var collision_shape: CollisionShape2D = $Collision
 
 func _physics_process(delta: float) -> void:
-    var movement := Vector2.ZERO
+    var screen_size = get_viewport_rect().size
+    var paddle_size = collision_shape.get_shape().get_rect().size
+
+    var paddle_min_x = paddle_margin + paddle_size.x / 2
+    var paddle_max_x = screen_size.x - paddle_size.x / 2 - paddle_margin
 
     if Input.is_action_pressed("PaddleLeft"):
-        movement.x = - speed * delta
+        position.x += - speed * delta
     elif Input.is_action_pressed("PaddleRight"):
-        movement.x = speed * delta
+        position.x += speed * delta
 
-    var collision := move_and_collide(movement)
-    if collision:
-        resolve_collision(collision)
-
-func resolve_collision(collision: KinematicCollision2D) -> void:
-    position.x -= collision.get_remainder().x
+    position.x = clamp(position.x, paddle_min_x, paddle_max_x)
