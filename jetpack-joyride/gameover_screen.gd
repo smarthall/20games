5644072837e0fd8cc337b120092042b9621e24ff
Daniel Hall <git@danielhall.me>
2025-08-19 20:07:49 +1000

    Exit game when escape pressed on gameover screen

--- jetpack-joyride/gameover_screen.gd
+++ jetpack-joyride/gameover_screen.gd
@@ -19,3 +19,7 @@ func _on_exit_button_pressed() -> void:
 
 func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		_on_exit_button_pressed()
