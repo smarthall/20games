ba9965827849a952d196ef57ef9c5ebb76871aba
Daniel Hall <git@danielhall.me>
2025-08-19 13:48:55 +1000

    Implement code for pickups

--- jetpack-joyride/infinite_level.gd
+++ jetpack-joyride/infinite_level.gd
@@ -16,6 +16,14 @@ const BACKGROUND_TILE_WEIGHTS: Dictionary[int, Vector2i] = {
 	99: BACKGROUND_TILE_MUSHROOM,
 }
 
+const PICKUP_TILE_BRONZE_COIN = "bronze"
+const BRONZE_VALUE = 1
+const PICKUP_TILE_SILVER_COIN = "silver"
+const SILVER_VALUE = 3
+const PICKUP_TILE_GOLD_COIN = "gold"
+const GOLD_VALUE = 5
+const PICKUP_TILE_HEART = "heart"
+
 func randomize_background_timemap(tm: TileMapLayer) -> void:
 	for x in range(tm.get_used_rect().size.x):
 		var tile_position := Vector2i(x, 1)
@@ -30,6 +38,12 @@ func get_random_background_tile() -> Vector2i:
 			return BACKGROUND_TILE_WEIGHTS[weight]
 
 	return BACKGROUND_TILE_WEIGHTS[BACKGROUND_TILE_WEIGHTS.keys()[0]]
+
+func heart_pickup() -> void:
+	if hud.hp < 6:
+		hud.hp += 1
+	else:
+		player.invincible(2.0)
 
 func _process(delta: float) -> void:
 	hud.distance += (delta * level_scroller.speed) / 100
@@ -46,8 +60,18 @@ func _on_player_hazard_collision() -> void:
 		get_tree().quit()
 
 func _on_player_pickup_collision(body: TileMapLayer, coords: Vector2i):
+	var type = body.get_cell_tile_data(coords).get_custom_data("type")
+
 	print("Player picked up item from ", body, " at ", coords)
+	print("Item Type: ", type)
 
 	body.erase_cell(coords)
 
-	hud.coins += 1
+	if type == PICKUP_TILE_HEART:
+		heart_pickup()
+	elif type == PICKUP_TILE_BRONZE_COIN:
+		hud.coins += BRONZE_VALUE
+	elif type == PICKUP_TILE_SILVER_COIN:
+		hud.coins += SILVER_VALUE
+	elif type == PICKUP_TILE_GOLD_COIN:
+		hud.coins += GOLD_VALUE
